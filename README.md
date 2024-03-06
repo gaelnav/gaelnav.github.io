c2409da523efd1091c60463d0537484c8c1c5cde
Grxson <159930375+gaelnav@users.noreply.github.com>
2024-03-06 06:17:50 -0600

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# gaelnav.github.io
--- script.js
+++ script.js
@@ -1,25 +1,70 @@
-$(document).ready(function(){
- 
+const pictures = document.querySelectorAll('.Picture');
+var previousTouch = undefined;
 
-  $('.valentines-day').click(function(){
-      // Animación de desvanecimiento de los elementos del sobre
-      $('.envelope').css({'animation':'fall 3s linear 1', '-webkit-animation':'fall 3s linear 1'});
-      $('.envelope').fadeOut(800, function() {
-          // Ocultar elementos dentro de .valentines-day
-          $('.valentines-day .heart, .valentines-day .text, .valentines-day .front').hide();
+function updateElementPosition(element, event) {
+  var movementX, movementY;
 
-          // Hacer visible la carta con una animación ondulante
-          $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
-          $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
-              var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
-              $(this).css('transform', 'scale(' + scale + ')');
-          }}); // Animación de ondulación
-      });
-  });
+  if (event.type === 'touchmove') {
+    const touch = event.touches[0];
+    movementX = previousTouch ? touch.clientX - previousTouch.clientX : 0;
+    movementY = previousTouch ? touch.clientY - previousTouch.clientY : 0;
+    previousTouch = touch;
+  } else {
+    movementX = event.movementX;
+    movementY = event.movementY;
+  }
+  
+  const elementY = parseInt(element.style.top || 0) + movementY;
+  const elementX = parseInt(element.style.left|| 0) + movementX;
+
+  element.style.top = elementY + "px";
+  element.style.left = elementX + "px";
+}
+
+function startDrag(element, event) {
+  const updateFunction = (event) => updateElementPosition(element, event);
+  const stopFunction = () => stopDrag({update: updateFunction, stop: stopFunction});
+  document.addEventListener("mousemove", updateFunction);
+  document.addEventListener("touchmove", updateFunction);
+  document.addEventListener("mouseup", stopFunction);
+  document.addEventListener("touchend", stopFunction);
+}
+
+function stopDrag(functions) {
+  previousTouch = undefined;
+  document.removeEventListener("mousemove", functions.update);
+  document.removeEventListener("touchmove", functions.update);
+  document.removeEventListener("mouseup", functions.stop);
+  document.removeEventListener("touchend", functions.stop);
+}
+
+pictures.forEach(picture => {
+  const range = 100;
+  const randomX = Math.random() * (range * 2) - range;
+  const randomY = Math.random() * (range * 2) - range;
+  const randomRotate = Math.random() * (range / 2) - range / 4;
+  const startFunction = (event) => startDrag(picture, event);
+  picture.style.top = `${randomY}px`;
+  picture.style.left = `${randomX}px`;
+  picture.style.transform = `translate(-50%, -50%) rotate(${randomRotate}deg)`;
+  picture.addEventListener("mousedown", startFunction);
+  picture.addEventListener("touchstart", startFunction);
 });
 
-    (function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-      .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-      n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-      (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-      ml('account', '736768');
+
+
+$('.hero-btn').click(function(){
+  if(!$('.hero-btn').parent().hasClass('active')){
+    $(this).parent().stop().addClass('active');
+    setTimeout(function(){  
+      $('.hero-btn').parent().removeClass('active'); 
+      mostrarTextoAmor(); 
+    }, 2500);
+  }
+});
+
+function mostrarTextoAmor() {
+  // Cambia la ubicación de la página actual a 'galeria.html'
+  window.location.href = "galeria.html";
+}
+
